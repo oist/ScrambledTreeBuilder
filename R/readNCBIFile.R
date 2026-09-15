7e0ca070dfb35acf4ed4c7791bfbe0c0a20a3d35
pribiller <pribiller@gmail.com>
2026-09-16 01:29:41 +0900

    Refactor code Substantial refactoring of the two new functions, `getNCBIinfo` and `readNCBIfile`.

--- R/readNCBIFile.R
+++ R/readNCBIFile.R
@@ -1,11 +1,10 @@
-#' Gets the NCBI info for a given node.
+#' Gets the NCBI info for a given node label.
 #'
-#' @param node Node
-#' @param your_tibble Tibble data developed from tree.
-#' @param your_ncbidf Dataframe containing the NCBI info from `ncbi_datasets.tsv`.
-#' @param colname The desired column from the NCBI dataframe.
+#' @param node_label The label from the desired node (e.g. "Callithrix_jacchus_GCA_049354715.1").
+#' @param tree A [`ConvenientTblTree`] object.
+#' @param ncbi_df Dataframe containing the NCBI info from `ncbi_datasets.tsv`.
 #'
-#' @return The row in the dataframe corresponding to the specified node.
+#' @return The row in the dataframe corresponding to the specified node label.
 #'
 #' @family Functions for trees
 #'
@@ -13,8 +12,8 @@
 #'
 #' @export
 #'
-getNCBIinfo <- function(node, your_tibble, your_ncbidf, colname) {
-  your_ncbidf[paste(gsub(" ", "_", your_ncbidf$Organism.Name), your_ncbidf$Assembly.Accession, sep = "_") == node,][[colname]]
+getNCBIinfo <- function(node_label, tree, ncbi_df) {
+  ncbi_df[paste(gsub(" ", "_", ncbi_df$Organism.Name), ncbi_df$Assembly.Accession, sep = "_") == node_label,]
 }
 
 
@@ -23,10 +22,10 @@ getNCBIinfo <- function(node, your_tibble, your_ncbidf, colname) {
 #' Adds new node columns data in a tree, containing the NCBI information
 #' saved in the file `ncbi_datasets.tsv`.
 #'
-#' @param your_tibble A [`ConvenientTblTree`] object.
-#' @param input_file Path to the file `ncbi_datasets.tsv`.
+#' @param tree A [`ConvenientTblTree`] object.
+#' @param ncbi_file Path to the file `ncbi_datasets.tsv`.
 #'
-#' @return The `your_tibble` object with 3 additional columns:
+#' @return The `tree` object with 3 additional columns:
 #'   * `NCBI_id`, containing the NCBI identifier;
 #'   * `NCBI_nbchr`, containing the number of chromosomes; and
 #'   * `NCBI_seqlen`, containing the sequence length.
@@ -35,24 +34,25 @@ getNCBIinfo <- function(node, your_tibble, your_ncbidf, colname) {
 #'
 #' @author Priscila Biller
 #'
+#' @importFrom utils read.table
 #' @export
 #'
-readNCBIfile <- function(your_tibble, input_file) {
+readNCBIfile <- function(tree, ncbi_file) {
   # Read file with NCBI info.
-  if (!file.exists(input_file)) {
-    stop(paste("File does not exist: ", input_file))
+  if (!file.exists(ncbi_file)) {
+    stop(paste("File does not exist: ", ncbi_file))
   }
-  df_ncbi <- read.table(file=input_file, sep='\t', header=TRUE)
+  ncbi_df <- read.table(file=ncbi_file, sep='\t', header=TRUE)
   # Columns to be added for every node in the tree.
-  desired_columns <- list(list(ncbi_name="Organism.Taxonomic.ID",                       tibble_name="NCBI_id"),
-                          list(ncbi_name="Assembly.Stats.Total.Number.of.Chromosomes",  tibble_name="NCBI_nbchr"),
-                          list(ncbi_name="Assembly.Stats.Total.Sequence.Length",        tibble_name="NCBI_seqlen"))
+  desired_columns <- list(list(ncbi_name="Organism.Taxonomic.ID",                       tree_name="NCBI_id"),
+                          list(ncbi_name="Assembly.Stats.Total.Number.of.Chromosomes",  tree_name="NCBI_nbchr"),
+                          list(ncbi_name="Assembly.Stats.Total.Sequence.Length",        tree_name="NCBI_seqlen"))
   # Add NCBI info for every node in the tree with a label (e.g. "Callithrix_jacchus_GCA_049354715.1")
-  node_labels <- your_tibble$label[!is.na(your_tibble$label)] |> purrr::set_names()
+  nodes <- tree$label[!is.na(tree$label)] |> purrr::set_names() |> purrr::map(getNCBIinfo, tree=tree, ncbi_df=ncbi_df)
+  print(nodes)
   for(newcol in desired_columns){
-    node_info <- node_labels |> sapply(getNCBIinfo, your_tibble, df_ncbi, newcol$ncbi_name)
-    your_tibble[, newcol$tibble_name] <- NA
-    your_tibble[match(names(node_info), your_tibble$label), newcol$tibble_name] <- unname(node_info)
+    tree[, newcol$tree_name] <- NA
+    tree[match(names(nodes), tree$label), newcol$tree_name] <- (nodes |> purrr::map_vec(\(row) row[[newcol$ncbi_name]]))
   }
-  your_tibble
+  tree
 }
